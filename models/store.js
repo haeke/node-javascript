b4fc37234b55f522c272d7bf24382c44d812c24d
Edwin Rivera <waehake@gmail.com>
2017-08-15 00:10:36 -0400

    added stores and mixins

---
+++ models/store.js
@@ -0,0 +1,36 @@
+// we are going to use strict mode
+// requires that we define the meta data before we can populate the DB with data
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const slug = require('slugs');
+
+// make the schema
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Enter a store name',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+//pre save hook - set the slug property
+storeSchema.pre('save', function (next) {
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; //stop the function
+  }
+  // set the slug property
+  this.slug = slug(this.name);
+  next();
+  //TODO make slugs more unique
+});
+
+// reference for outside of this file
+module.exports = mongoose.model('Store', storeSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 // route specific middleware example
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
+
+//add store controller
+router.get('/add', storeController.addStore);
+
+//post items from the editstore page
+router.post('/add', storeController.createStore);
 
 module.exports = router;
