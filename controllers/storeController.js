ae3723e5a315b11e8ff9c9a0ee32a6668a693a11
Edwin Rivera <waehake@gmail.com>
2017-08-15 21:31:46 -0400

    added a flash message and update aync await and added a redirect to the corredsponding store route

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
-  req.flash('error', 'An error occured');
-  req.flash('info', 'Information for what just happened');
-  req.flash('warning', 'You might not want to do that');
-  req.flash('success', 'That worked');
   res.render('index');
 };
 
@@ -17,9 +12,7 @@ exports.addStore = (req, res) => {
 //use async await to add data into the mongoDB database
 exports.createStore = async (req, res) => {
   //create the store with mongoDB
-  const store = new Store(req.body);
-  await store.save();
+  const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
-  console.log('finished');
-  res.redirect('/');
+  res.redirect(`/store/${store.slug}`);
 };
