7f7a94e826f4dd3130add39fa06c1a32e45254cb
Edwin Rivera <waehake@gmail.com>
2017-08-20 19:09:21 -0400

    added geocoding function with google maps

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,7 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+// where webpack bundles
+import autocomplete from './modules/autocomplete';
+
+autocomplete($('#address'), $('#lat'), $('#lng'));
---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,13 @@
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; //skip if there's no input
+  const dropdown = new google.maps.places.Autocomplete(input);
+
+  //
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+};
+
+export default autocomplete;
