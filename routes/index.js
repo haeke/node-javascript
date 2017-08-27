55065ed83990adf48bab9d2680932e3d62270ac0
Edwin Rivera <waehake@gmail.com>
2017-08-27 18:03:01 -0400

    added ability to update account profile

--- routes/index.js
+++ routes/index.js
@@ -50,6 +50,7 @@ router.post('/register',
 router.get('/logout', authController.logout);
 
 //route for account page
-router.get('/account', userController.account);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
