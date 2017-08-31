3972561732059addaa3769424aa61bd68733957f
Edwin Rivera <waehake@gmail.com>
2017-08-30 20:31:10 -0400

    modified search to query stores using the json api route created

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -8,7 +8,22 @@ function typeAhead(search) {
   const searchInput = search.querySelector('input[name="search"]');
   const searchResults = search.querySelector('.search__results');
 
-  console.log(searchInput, searchResults);
+  //addeventlistener to track what is being typed into the input
+  searchInput.on('input', function () {
+    if (!this.value) {
+      //no value remove searchresults
+      searchResults.style.display = 'none';
+      return; //stop listener
+    }
+
+    //show the search result
+    searchResults.style.display = 'block';
+
+    axios.get(`/api/v1/search?q=${this.value}`)
+          .then(res => {
+            console.log(res.data);
+          })
+  });
 };
 
 export default typeAhead;
