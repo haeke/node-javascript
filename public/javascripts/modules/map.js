c08e4f38b76c9cd4259f38116003422e7b441f6d
Edwin Rivera <waehake@gmail.com>
2017-09-02 21:40:27 -0400

    updated loadPlaces and makeMap to return the coordinates for stores near given lat and lng

--- public/javascripts/modules/map.js
+++ public/javascripts/modules/map.js
@@ -1,11 +1,35 @@
 const axios = require('axios');
 
-function loadPlaces(map, lat = 43.2, lng= -79.8) {
+import { $ } from './bling';
 
+const mapOptions = {
+  center: { lat: 43.2, lng: -79.8 },
+  zoom: 8,
+};
+
+function loadPlaces(map, lat = 43.2, lng= -79.8) {
+  axios.get(`api/stores/near?lat=${lat}&lng=${lng}`)
+    .then(res => {
+      const places = res.data;
+      if (!places.length) {
+        alert('no places found');
+        return;
+      }
+
+      const markers = places.map(place => {
+        const [placeLng, placeLat] = place.location.coordinates;
+        console.log(placeLng, placeLat);
+      });
+    });
 }
 
 function makeMap(mapDiv) {
   if (!mapDiv) return;
+  //create the map
+  const map = new google.maps.Map(mapDiv, mapOptions);
+  loadPlaces(map);
+  const input = $('[name="geolocate"]');
+  const autocomplete = new google.maps.places.Autocomplete(input);
 };
 
 export default makeMap;
