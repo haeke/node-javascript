3e18c07e0d0da8325f252938d646da5cad9d4464
Edwin Rivera <waehake@gmail.com>
2017-09-04 18:02:45 -0400

    updated the getTopStores function to include the top ten stores with review fields limited to 10

--- models/store.js
+++ models/store.js
@@ -90,6 +90,19 @@ storeSchema.statics.getTopStores = function() {
     //lookup stores and populate the reviews
     { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' } },
     //filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true} } },
+    //add the average reviews field
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating' },
+    }},
+    //sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    //limit to 10
+    { $limit: 10 },
   ]);
 };
 
